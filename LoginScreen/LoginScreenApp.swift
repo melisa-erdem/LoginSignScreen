8029773228916503769661ce6e89ca13bd8df9b3
melisa-erdem <melisaerdem.ceng@gmail.com>
2024-04-23 15:30:58 +0300

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Melisa Erdem on 23.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
